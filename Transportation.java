0d6d31918b3b670d987a3c15a24583037aed4658
MatthewSB19 <115648310+MatthewSB19@users.noreply.github.com>
2022-10-12 11:21:43 -0400

    Add files via upload

---
+++ Transportation.java
@@ -0,0 +1,40 @@
+public class Transportation {
+    private String purchaseType;
+    private int speed;
+    private int numPassengers;
+    private double cost;
+
+    public Transportation(String purchaseType, int speed, int numPassengers, double cost) {
+        this.purchaseType = purchaseType;
+        this.speed = speed;
+        this.numPassengers = numPassengers;
+        this.cost = cost;
+    }
+
+    public String getPurchaseType() {
+        return purchaseType;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+    public double getCost() {
+        return cost;
+    }
+
+    public void setPurchaseType(String purchaseType) {
+        this.purchaseType = purchaseType;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setNumPassengers(int numPassengers) {
+        this.numPassengers = numPassengers;
+    }
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
