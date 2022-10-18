cb3da1dc34840dcdc1ea4479ffd4d1ad68b90689
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-10-18 19:04:09 -0400

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -1,19 +1,25 @@
 public class Plane extends AirTransportation{
-    private String engine;
+    private int numEngine;
     private int wingspan;
 
 
 
-    public Plane(int speed, double cost, String purchaseType, String numPassengers, String engine, int wingspan){
-        this.engine = engine;
+    public Plane(int speed, double cost, String purchaseType, String numPassengers, String engineType, int numEngine, int wingspan){
+        super(speed, cost, purchaseType, numPassengers, engineType);
+        this.numEngine = numEngine;
         this.wingspan = wingspan;
     }
 
-    public String getEngine() { return engine; }
+    public int getNumEngine() { return numEngine; }
 
     public int getWingspan() { return wingspan; }
 
-    public void setEngine(String engine) { this.engine = engine; }
+    public void setNumEngine(int numEngine) { this.numEngine = numEngine; }
 
     public void setWingspan(int wingspan) { this.wingspan = wingspan;}
+
+    @Override
+    public String toString(){
+        return String.format("%sNumber of Engines:%-12dWingspan:%-10d%n",super.toString(), numEngine, wingspan);
+    }
 }
