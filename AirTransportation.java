29d34e474875892b088c57400cbc3cb72da5e9fc
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-10-21 10:41:43 -0400

    Update AirTransportation.java

--- AirTransportation.java
+++ AirTransportation.java
@@ -1,3 +1,11 @@
+/**
+ * Adam Eichman
+ * 10/21/2022
+ * CSC 331-001
+ * AirTransportation
+ * Class that is to be used as a super class to get more specific forms of air transportation
+ */
+
 public class AirTransportation extends Transportation {
     private String engineType;
     public AirTransportation(int speed, double cost, String purchaseType, int numPassengers, String engineType) {
