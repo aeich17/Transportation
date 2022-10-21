398d830b99b7fa21f1549f86f42dd72588758556
JMaveus <99935531+JMaveus@users.noreply.github.com>
2022-10-21 10:38:06 -0400

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,4 +1,10 @@
-public class Bike extends LandTransportation{
+/**
+ * PROGRAM PURPOSE: Represents a bike that is available to be rented.
+ * Name: Joey Maveus
+ * Date: 10/18/2022
+ * Section: CSC 331-001
+*/
+public class Bike extends LandTransportation{//extends the land transportation class while adding unique elements to the class to distinguish itself.
     private int numGears;
     private String bikeType;
 
@@ -24,13 +30,13 @@ public class Bike extends LandTransportation{
         this.bikeType = bikeType;
     }
 
-    public String location(){
+    public String location(){//Gives a specific location to pick up the bike
         String bikeLocation = "Go to the Seahawk Bike stand to get your bike for the day.";
         return bikeLocation;
     }
 
     @Override
-    public String toString(){
+    public String toString(){//supers in the toSting methods of both Transportation and LandTransportation to print all of the detailed info.
         return String.format("%sGears:%-12dBike Type:%-10s%n",super.toString(),numGears,bikeType);
     }
 }
