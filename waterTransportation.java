fc2e9e03238c84def5cf385d189fe0269ea008f9
MatthewSB19 <115648310+MatthewSB19@users.noreply.github.com>
2022-10-21 12:23:26 -0400

    Update waterTransportation.java

--- waterTransportation.java
+++ waterTransportation.java
@@ -1,44 +1,12 @@
-public class Ship extends Transportation{
-    private String purpose;
-    private double maxCargo;
+public class waterTransportation extends Transportation{
 
-    public Ship(String purchaseType, int speed, int numPassengers, double cost, String purpose, double maxCargo) {
+    waterTransportation(String purchaseType, int speed, int numPassengers, double cost){
         super(purchaseType, speed, numPassengers, cost);
-        this.purpose = purpose;
-        this.maxCargo = maxCargo;
-    }
-
-    public String getPurpose() {
-        return purpose;
-    }
-    public double getMaxCargo() {
-        return maxCargo;
-    }
-    public void setPurpose(String purpose) {
-        this.purpose = purpose;
-    }
-    public void setMaxCargo(double maxCargo) {
-        this.maxCargo = maxCargo;
-    }
-
-    public double totalCost(){
-        int numPassengers = getNumPassengers();
-        double costPerTicket = getCost();
-        if (getPurchaseType().equals("Rent")){
-            return costPerTicket;
-        } else {
-            double finalTotalCost = numPassengers * costPerTicket;
-            return finalTotalCost;
-        }
-    }
-
-    public String location(){
-        String shipLocation = "Go to Port Wilmington at 12:00pm.";
-        return shipLocation;
     }
 
     @Override
-    public String toString(){
-        return String.format("%sPurpose: %-10s%nMax Cargo: %-10s%n",super.toString(),purpose,maxCargo);
+    public String toString() {
+        return String.format("%s",
+                super.toString());
     }
 }
